dcd1f242c8070ab3eacd6deba3701c2772e042b9
Elly Maimon <eliranmaimon@gmail.com>
2019-09-12 11:06:51 -0700

    API Update and Delete

--- CretaceousPark/Controllers/AnimalsController.cs
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using CretaceousPark.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CretaceousPark.Controllers
 {
@@ -36,6 +37,24 @@ namespace CretaceousPark.Controllers
     public ActionResult<Animal> Get(int id)
     {
       return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+    }
+
+    // PUT api/animals/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Animal animal)
+    {
+      animal.AnimalId = id;
+      _db.Entry(animal).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    // DELETE api/animals/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var animalToDelete = _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+      _db.Animals.Remove(animalToDelete);
+      _db.SaveChanges();
     }
   }
 }
